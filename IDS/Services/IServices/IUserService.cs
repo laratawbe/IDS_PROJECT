e3e4f561d2d2079319f0d1e8592bec8d7d01fe5b
laratawbe <115413984+laratawbe@users.noreply.github.com>
2024-01-20 21:52:29 +0200

    IUserService definition 1

--- IDS/Services/IServices/IUserService.cs
+++ IDS/Services/IServices/IUserService.cs
@@ -1,6 +1,19 @@
-﻿namespace IDS.Services.IServices
+﻿using System.Linq.Expressions;
+using IDS.Models;
+using IDS.Models.DTOs;
+
+namespace IDS.Services.IServices
 {
     public interface IUserService
+
     {
+        Task<List<User>> GetAllUsersAsync(string? category = null, string? search = null, int pageSize = 0, int pageNumber = 1,string? sortBy = null, bool ascending = true);
+        Task<User> GetUserByIdAsync(int userId);
+        Task<User> CreateUserAsync(User user);
+        Task<bool> DeleteUserAsync(int id);
+        Task SaveAsync();
+
+        Task UpdateAsync(int id, User updatedUser);
     }
 }
+
--- IDS/Services/UserService.cs
+++ IDS/Services/UserService.cs
@@ -1,6 +1,45 @@
-﻿namespace IDS.Services
+﻿using Azure;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using System.Net;
+using IDS.Models;
+using IDS.Models.DTOs;
+using IDS.Repository.IRepository;
+using IDS.Services.IServices;
+
+namespace IDS.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        public Task<User> CreateUserAsync(User user)
+        {
+           
+        }
+
+        public Task<bool> DeleteUserAsync(int id)
+        {
+            
+        }
+
+        public Task<List<User>> GetAllUsersAsync(string? category = null, string? search = null, int pageSize = 0, int pageNumber = 1, string? sortBy = null, bool ascending = true)
+        {
+            
+        }
+
+        public Task<User> GetUserByIdAsync(int userId)
+        {
+            
+        }
+
+        public Task SaveAsync()
+        {
+           
+        }
+
+        public Task UpdateAsync(int id, User updatedUser)
+        {
+
+        }
     }
 }
