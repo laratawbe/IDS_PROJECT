ebd40ec5b5aeb17f5227a895bde95f8871ba778d
HusseinCharafEddine <114979422+HusseinCharafEddine@users.noreply.github.com>
2024-01-24 12:55:07 +0200

    testasdaeras

--- IDS/Models/EventMember.cs
+++ IDS/Models/EventMember.cs
@@ -7,9 +7,9 @@ public partial class EventMember
 {
     public int EventMemberId { get; set; }
 
-    public int? EventId { get; set; }
+    public int EventId { get; set; }
 
-    public int? MemberId { get; set; }
+    public int MemberId { get; set; }
 
     public virtual Event? Event { get; set; }
 
